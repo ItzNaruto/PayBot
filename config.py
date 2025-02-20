a0b34d1eb978b0cfd748e7229eda414796c0a561
Naruto X <92075412+ItzNaruto@users.noreply.github.com>
2025-02-20 20:13:59 +0530

    .

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 API_ID = 11472991
 
 # Your api hash
-API_HASH = " c78c50d54baf2173e8b3f75c359c0c72 "
+API_HASH = "c78c50d54baf2173e8b3f75c359c0c72"
 
 # Your bot token from bot father
 BOT_TOKEN = "7653029123:AAGZT26o00FphPZloCohvv-QI5-jFKIMBH4"
@@ -17,7 +17,7 @@ UPI_ID = "sakthirajtnbotz@fam"
 LOG_CHANNEL = -1002286569015
 
 # Mod's id (user id)
-MODS_ID = [1430742022]
+MODS_ID = [1430742022, 6495253163]
 
 # Mongodb URL
-MONGODB_URL = "mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority "
+MONGODB_URL = "mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority"
