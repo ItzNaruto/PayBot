fe16e57dc90952c458a2f9858bda838893a8ae1b
Naruto X <92075412+ItzNaruto@users.noreply.github.com>
2025-02-20 19:51:53 +0530

    .

---
+++ config.py
@@ -0,0 +1,23 @@
+# Your api hash
+API_ID = 123456789
+
+# Your api hash
+API_HASH = " "
+
+# Your bot token from bot father
+BOT_TOKEN = "7653029123:AAGZT26o00FphPZloCohvv-QI5-jFKIMBH4"
+
+# Your qr code link
+QR_PIC_URL = "https://envs.sh/dhQ.jpg"
+
+# Your upi id
+UPI_ID = "sakthirajtnbotz@fam"
+
+# Log channel id
+LOG_CHANNEL = -1002286569015
+
+# Mod's id (user id)
+MODS_ID = [6495253163, 1430742022]
+
+# Mongodb URL
+MONGODB_URL = " "
